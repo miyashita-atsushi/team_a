d33067f4545f7bd34a18b2aa29d969482ff2bac0
takeshi <garyuu142@gmail.com>
2020-05-18 17:26:55 +0900

    takeshi
    fericaApp first commit

---
+++ development/felica/TestCords/FericaReader/Card/Suica.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Linq.Mapping;
+
+namespace FericaReader
+{
+    /// <summary>
+    /// Suica以外を読み込むため、実装時は基底クラスを作成し継承する
+    /// 
+    /// ここで実装しているクラスは取得バイト列のフォーマットが駅改札の場合のみを考えています
+    /// バス利用・物販などは処理ごとで日付、残高などのフォーマットが異なる為
+    /// 実装時は履歴内のProcess・Terminalで分岐を判断する
+    /// </summary>
+    [Table(Name = "UserHistory")]
+     class Suica : ICCard
+    {
+        public const int SERVICE_SUICA_INOUT = 0x108f;
+        //履歴読込のサービスコード
+        public const int SERVICE_SUICA_HISTORY = 0x090f;
+
+        public int TerminalCode      ;
+        public int ProcessCode       ;
+        public int GetStation        ;
+        public int InStationLineCode ;
+        public int InStationCode     ;
+        public int OutStationLineCode;
+        public int OutStationCode    ;
+        //public int Balance           ;
+        public int Seq               ;
+        public int Region            ;
+        //public int TerminalCode         { get; set; }//byte[0]　端末種
+        //public int ProcessCode          { get; set; }//byte[1]　処理
+        //public int GetStation       { get; set; }//byte[3]　入出場種別
+        //public DateTime Date        { get; set; }//byte[4,5]　日付　bitで(7/4/5)(年/月/日)
+        //public int InStationLineCode    { get; set; }//byte[6]　入線区
+        //public int InStationCode        { get; set; }//byte[7]　入駅
+        //public int OutStationLineCode   { get; set; }//byte[8]　出線区
+        //public int OutStationCode       { get; set; }//byte[9]  出駅
+        //public int Balance          { get; set; }//byte[10,11]　残高(little endian)
+        //public int Seq              { get; set; }//byte[12,13,15]　連番
+        //public int Region           { get; set; }//byte[15]　地域コード
+
+        /// <summary>
+        /// 処理
+        /// </summary>
+        public enum PROCESS_NUMBERS : byte
+        {
+            運賃              = 1,
+            チャージ          = 2,
+            券購1             = 3,
+            清算              = 5,
+            窓出              = 6,
+            新規              = 7,
+            控除              = 8,
+            バス_Pitapa       = 13,
+            バス_Iruca        = 15,
+            再発行            = 17,
+            支払              = 19,
+            オートチャージ    = 21,
+            バスチャージ      = 31,
+            券購2             = 35,
+            物販              = 70,
+            特典              = 72,
+            入金              = 73,
+            物販取消          = 74,
+            入場物販          = 75,
+            精算_他社         = 132,
+            精算_他社入場     = 133,
+            物販_現金併用     = 198,
+            物販_入場現金併用 = 203
+        }
+        public enum TERMINAL_NUMBERS : byte
+        {
+            精算機           =3 ,
+            携帯型端末       =4 ,
+            車載端末         =5 ,
+            券売機1            =7 ,
+            券売機2           =8 ,
+            入金機           =9 ,
+            券売機          =18 ,
+            券売機等1        =20 ,
+            券売機等2        =21 ,
+            改札機          =22 ,
+            簡易改札機      =23 ,
+            窓口端末1        =24,
+            窓口端末2        =25 ,
+            改札端末        =26 ,
+            携帯電話        =27 ,
+            乗継精算機      =28 ,
+            連絡改札機      =29 ,
+            簡易入金機      =31 ,
+            VIEWALTTE1      =70 ,
+            VIEWALTTE2      =72 ,
+            物販端末       =199 ,
+            自販機         =200 
+
+        }
+
+        private const int CT_SHOP = 199;  // 物販端末
+        private const int CT_VEND = 200;  // 自販機
+        private const int CT_CAR = 5;   // 車載端末(バス)
+        /// <summary>
+        ///重複する値があるのでこちらを使用
+        /// </summary>
+        /// <param name="ctype"></param>
+        private string consoleType(int ctype)
+        {
+            switch (ctype) {
+            //case CT_SHOP: return "物販端末";
+            //case CT_VEND: return "自販機";
+            //case CT_CAR: return "車載端末";
+            case 199: return "物販端末";
+            case 200: return "自販機";
+            case 5: return "車載端末";
+            case 3: return "清算機";
+            case 4: return "携帯型端末";
+            case 8: return "券売機";
+            case 9: return "入金機";
+            case 18: return "券売機";
+            case 20:
+            case 21: return "券売機等";
+            case 22: return "改札機";
+            case 23: return "簡易改札機";
+            case 24:
+            case 25: return "窓口端末";
+            case 26: return "改札端末";
+            case 27: return "携帯電話";
+            case 28: return "乗継精算機";
+            case 29: return "連絡改札機";
+            case 31: return "簡易入金機";
+            case 70:
+            case 72: return "VIEW ALTTE";
+            }
+            return "不明";
+        }
+
+        // 処理
+        public string procType(int proc)
+        {
+            switch (proc) {
+            case 1  : return "運賃";
+            case 2  : return "チャージ";
+            case 3  : return "券購";
+            case 4  : 
+            case 5  : return "清算";
+            case 6  : return "窓出";
+            case 7  : return "新規";
+            case 8  : return "控除";
+            case 13 : return "バス"; //PiTaPa系
+            case 15 : return "バス"; //IruCa系
+            case 17 : return "再発行";
+            case 19 : return "支払";
+            case 20 :
+            case 21 : return "オートチャージ";
+            case 31 : return "バスチャージ";
+            case 35 : return "券購";
+            case 70 : return "物販";
+            case 72 : return "特典";
+            case 73 : return "入金";
+            case 74 : return "物販取消";
+            case 75 : return "入場物販";
+            case 132: return "精算(他社)";
+            case 133: return "精算(他社入場)";
+            case 198: return "物販(現金併用)";
+            case 203: return "物販(入場現金併用)";
+            }
+            return "不明";
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Suica()
+        {
+
+        }
+
+        public void analyzeTransaction(byte[] data)
+        {
+            stCode = new StationCode();
+
+            this.TerminalCode = data[0];
+            this.ProcessCode = data[1];       
+            this.GetStation = data[3];    
+            this.InStationLineCode = data[6]; 
+            this.InStationCode = data[7]; 
+            this.OutStationLineCode = data[8];
+            this.OutStationCode = data[9];
+            this.Region = data[15];
+
+            this.Balance = read2l(data,10);
+
+            //this.Seq = data[];            ToDO:3byte読込メソッド作成
+
+            this.Date = AnaryzeDateTime(read2b(data,4));          
+            //this.ByteData =  BitConverter.ToString(data, 0, 16);
+            this.Terminal = consoleType(TerminalCode);
+            this.Process = procType(ProcessCode);
+            int inArea = getAreaCode(InStationLineCode,Region);
+            int outArea = getAreaCode(OutStationLineCode,Region);
+            this.InStationName = stCode.GetStationName(inArea,InStationLineCode,InStationCode);
+            this.OutStationName = stCode.GetStationName(outArea,OutStationLineCode,OutStationCode);
+
+            CreateTransactionID(data);
+        }
+
+        //2バイト読込(BigEndian)
+        protected int read2b(byte[] b, int pos)
+        {
+            int ret = b[pos] << 8 | b[pos + 1];
+            return ret;
+        }
+        //2バイト読込(littleEndian)
+         protected int read2l(byte[] b, int pos)
+        {
+            int ret = b[pos + 1] << 8 | b[pos];
+            return ret;
+        }
+        //public DateTime AnaryzeDateTime(int date)
+        //public string AnaryzeDateTime(int date)
+        public string AnaryzeDateTime(int date)
+        {
+            //年は2000年から数えて
+            int yy = (date >> 9) + 2000;
+            int mm = (date >> 5) & 0xf;
+            int dd = date & 0x1f;
+            try
+            {
+                
+                return new DateTime(yy, mm, dd).ToString("yyyy-MM-dd");
+                //return new DateTime(yy, mm, dd);
+            }
+            catch
+            {
+                //nullの場合
+                return new DateTime(0, 0, 0).ToString("yyyy-MM-dd");
+                //return new DateTime(0,0,0);
+            }
+        }
+        /// <summary>
+        /// 駅コードの為、エリアコード取得
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="region"></param>
+        /// <returns></returns>
+        private int getAreaCode(int line, int region)
+        {
+            var areacodeSoruce = (byte)(0x3 & region >> 4);
+            if ((line & 0xFF) < 128)
+                return 0;
+            if (region >= 0 && region <= 3)
+                return (byte)(region + 1);
+            return 0;
+        }
+        private void CreateTransactionID(byte[] data)
+        {
+
+            this.TransactionID = this.IDm ;
+            foreach(var d in data)
+            {
+                this.TransactionID += d.ToString("X2");
+            }
+        }
+
+    }
+}
---
+++ development/felica/TestCords/ReadPasori/MainWindow.xaml.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Protocols;
+using System.Web.UI;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Web.Services3.Messaging;
+using PCSC;
+using PCSC.Iso7816;
+
+namespace ReadPasori
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _selectedIndex = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonCheckReader_Click(object sender, RoutedEventArgs e)
+        {
+            textBox_Log.Clear(); 
+            comboBox_CardReader.Items.Clear();
+            //接続確立
+            using(var ctx = ContextFactory.Instance.Establish(SCardScope.User))
+            {
+                var firstReader = ctx.GetReaders().FirstOrDefault();
+                if (firstReader == null)
+                {
+                    textBox_Log.Text += "No reader connected."; //③
+                    return;
+                }
+                using (var reader = ctx.ConnectReader(firstReader, SCardShareMode.Direct, SCardProtocol.Unset))
+                {
+                    var status = reader.GetStatus();
+ 
+                    textBox_Log.Text += @"Reader names: {" + string.Join(", ", status.GetReaderNames()) + "}\r\n"; //④以下4行
+                    textBox_Log.Text += @"Protocol: {" + status.Protocol + "}\r\n";
+                    textBox_Log.Text += @"State: {" + status.State + "}\r\n";
+                    textBox_Log.Text += @"ATR: { " + BitConverter.ToString(status.GetAtr() ?? new byte[0]) + "}\r\n";
+                    textBox_Log.Text += @"ATR: { " + NoBarATR(status.GetAtr()) + "}\r\n";
+ 
+                    //comboBox_CardReader.Items.Add(status.GetReaderNames());//⑤
+                    status.GetReaderNames().ToList().ForEach(item => comboBox_CardReader.Items.Add(item));
+                }
+            }
+            textBox_Log.Text += "-----------------------------------------------\r\n"; //⑥
+        }
+
+        private void ComboBox_CardReader_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _selectedIndex = comboBox_CardReader.SelectedIndex;
+        }
+
+        private void Button_ReadID_Click(object sender, RoutedEventArgs e)
+        {
+            var contextFactory = ContextFactory.Instance;
+
+            using(var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                if (NoReaderFound(readerNames))
+                {
+                    textBox_Log.Text += "You need at least one reader in order to run this example." + "\r\n";
+                    return;
+                }
+
+                var  readerName = ChooseRfidReader(readerNames);
+
+                if(readerName == null) return;
+                using(var rfidReader = context.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.Any))
+                {
+                    var apdu = new CommandApdu(IsoCase.Case2Short, rfidReader.Protocol)
+                    {
+                        CLA = 0xFF,
+                        Instruction = InstructionCode.GetData,
+                        P1 = 0x00,
+                        P2 = 0x00,
+                        Le = 0
+                    };
+                    using (rfidReader.Transaction(SCardReaderDisposition.Leave))
+                    {
+                        textBox_Log.Text += "Retrieving the UID .... " + "\r\n";
+
+                        var sendPci         = SCardPCI.GetPci(rfidReader.Protocol);
+                        var receivePci      = new SCardPCI();
+                        var receiveBuffer   = new byte[256];
+                        var command         = apdu.ToArray();
+
+                        var bytesReceived   = rfidReader.Transmit(
+                            sendPci,
+                            command,
+                            command.Length,
+                            receivePci,
+                            receiveBuffer,
+                            receiveBuffer.Length);
+                        var responseApdu = new ResponseApdu(receiveBuffer, bytesReceived, IsoCase.Case2Short, rfidReader.Protocol);
+
+                        textBox_Log.Text += "SW1: " + responseApdu.SW1.ToString() + " ,SW2: " + responseApdu.SW2.ToString() +"\r\n";
+
+                        if (responseApdu.HasData)
+                        {
+                            textBox_Log.Text += "Uid: " + BitConverter.ToString(responseApdu.GetData()) + "\r\n";
+                        }
+                        else
+                        {
+                            textBox_Log.Text += "Uid: No uid received" + "\r\n";
+                        }
+                    }
+                }
+            }
+            textBox_Log.Text += "-----------------------------------------------\r\n";
+        }
+
+        private void Button_ReadCardType_Click(object sender, RoutedEventArgs e)
+        {
+            var contextFactory = ContextFactory.Instance;
+ 
+            using (var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                if (NoReaderFound(readerNames))
+                {
+                    textBox_Log.Text += "You need at least one reader in order to run this example." + "\r\n";
+                    return;
+                }
+ 
+                var readerName = ChooseRfidReader(readerNames);
+                if (readerName == null)
+                {
+                    return;
+                }
+ 
+                // 'using' statement to make sure the reader will be disposed (disconnected) on exit
+                using (var rfidReader = context.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.Any))
+                {
+                    var apdu = new CommandApdu(IsoCase.Case2Short, rfidReader.Protocol)
+                    {
+                        CLA = 0xFF,
+                        Instruction = InstructionCode.GetData,
+                        P1 = 0xF3,
+                        P2 = 0x00,
+                        Le = 0 // We don't know the ID tag size
+                    };
+ 
+                    using (rfidReader.Transaction(SCardReaderDisposition.Leave))
+                    {
+                        textBox_Log.Text += "Retrieving the UID .... " + "\r\n";
+ 
+                        var sendPci = SCardPCI.GetPci(rfidReader.Protocol);
+                        var receivePci = new SCardPCI(); // IO returned protocol control information.
+ 
+                        var receiveBuffer = new byte[256];
+                        var command = apdu.ToArray();
+ 
+                        var bytesReceived = rfidReader.Transmit(
+                            sendPci, // Protocol Control Information (T0, T1 or Raw)
+                            command, // command APDU
+                            command.Length,
+                            receivePci, // returning Protocol Control Information
+                            receiveBuffer,
+                            receiveBuffer.Length); // data buffer
+ 
+                        var responseApdu =
+                            new ResponseApdu(receiveBuffer, bytesReceived, IsoCase.Case2Short, rfidReader.Protocol);
+                        textBox_Log.Text += "SW1: " + responseApdu.SW1.ToString()
+                                                + ", SW2: " + responseApdu.SW2.ToString()
+                                                + "\r\n";
+                        if (responseApdu.HasData)
+                        {
+                            textBox_Log.Text += "CardType: " + BitConverter.ToString(responseApdu.GetData()) + "\r\n";
+                        }
+                        else
+                        {
+                            textBox_Log.Text += "Uid: No uid received" + "\r\n";
+                        }
+ 
+                    }
+                }
+            }
+            textBox_Log.Text += "-----------------------------------------------\r\n";
+        }
+
+        /// <summary>
+        /// 不要なものも含まれているので実装時は選別
+        /// 現在は履歴を一件ずつ解析、表示しているが処理金額比較などを行う為
+        /// データクラスを作成して、一度すべて読み込んでから解析を行う事
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_SuicaRead_Click(object sender, RoutedEventArgs e)
+        {
+            var contextFactory = ContextFactory.Instance;
+            using(var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                if (NoReaderFound(readerNames))
+                {
+                    textBox_Log.Text += "You need at least one reader in order to run this example.\r\n";
+                    return;
+                }
+                var readerName = ChooseRfidReader(readerNames);
+                if(readerName == null)return;
+
+                using(var rfidReader = context.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.Any))
+                {
+                    byte[] dataIn = { 0x0f,0x09 };
+                    var apduSelectFile = new CommandApdu(IsoCase.Case4Short, rfidReader.Protocol)
+                    {
+                        CLA = 0xFF,
+                        Instruction = InstructionCode.SelectFile,
+                        P1 = 0x00,
+                        P2 = 0x01,
+                        Data = dataIn,
+                        Le = 0
+                    };
+                    using (rfidReader.Transaction(SCardReaderDisposition.Leave))
+                    {
+                        textBox_Log.Text += "SelectFile .... \r\n";
+
+                        var sendPci = SCardPCI.GetPci(rfidReader.Protocol);
+                        var receivePci = new SCardPCI();
+                        var receiveBuffer = new byte[256];
+                        var command = apduSelectFile.ToArray();
+
+                        var bytesReceivedSelectedFile = rfidReader.Transmit(sendPci,command,command.Length,receivePci,receiveBuffer,receiveBuffer.Length);
+                        var responseApdu = new ResponseApdu(receiveBuffer,bytesReceivedSelectedFile,IsoCase.Case2Short,rfidReader.Protocol);
+
+                        textBox_Log.Text += "SW1: " + responseApdu.SW1.ToString()
+                                         + ", SW2: " + responseApdu.SW2.ToString()+ "\r\n"
+　　　　　　　　　　　　　　　　　　　　 + "Length: " + responseApdu.Length.ToString() + "\r\n";
+
+                        for (int i = 0; i < 20; ++i)
+                        {
+                            var apduReadBinary = new CommandApdu(IsoCase.Case2Short, rfidReader.Protocol)
+                            {
+                                CLA = 0xFF,
+                                Instruction = InstructionCode.ReadBinary,
+                                P1 = 0x00,
+                                P2 = (byte)i,
+                                Le = 0
+                            };
+                            textBox_Log.Text += "Read Binary .... \r\n";
+
+                            var commandReadBinary = apduReadBinary.ToArray();
+
+                            var bytesReceivedReadBinary2 =
+                                rfidReader.Transmit(sendPci, commandReadBinary, commandReadBinary.Length, receivePci, receiveBuffer, receiveBuffer.Length);
+                            var responseApdu2 = new ResponseApdu(receiveBuffer, bytesReceivedReadBinary2, IsoCase.Case2Extended, rfidReader.Protocol);
+
+                            textBox_Log.Text += "SW1: " + responseApdu2.SW1.ToString()
+                                        + ", SW2: " + responseApdu2.SW2.ToString()
+                                        + "\r\n"
+                                        + "Length: " + responseApdu2.Length.ToString() + "\r\n";
+
+                            //parse_tag(receiveBuffer);
+                            DataManager.GetInstance().AddHistryList(receiveBuffer);
+
+                            textBox_Log.Text += "\r\n";
+                        }
+                        //履歴DB、CSVに書き込み
+                        DataManager.GetInstance().WriteUserHistoryDB();
+                        foreach (var s in DataManager.GetInstance().GetHistoryList())
+                        {
+                            ShowResultData(s);
+                        }
+                    }
+                }
+            }
+            textBox_Log.Text += "-----------------------------------------------\r\n";
+        }
+
+        //ATR(Answer To Reset)ICカードの仕様定義
+        private string NoBarATR(byte[] arr)
+        {
+            if(arr == null)return string.Empty;
+            string result = string.Empty;
+
+            foreach (var oneByte in arr)
+            {
+                result += String.Format("{0:X2}",oneByte) + " ";
+            }
+            return result;
+        }
+
+        private bool NoReaderFound(ICollection<string> readerNames)
+        {
+            if(readerNames == null || readerNames.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        //コンボボックスで選択したカードリーダー名を取得、チェック
+        private string ChooseRfidReader(IList<string> readerNames)
+        {
+            textBox_Log.Text += "Available readers: ";
+            for(var i = 0 ;i < readerNames.Count ; i++)
+            {
+                textBox_Log.Text += "ReaderID: " + i.ToString() + " ,Reader Name:" + readerNames[i] + "\r\n";
+            }
+            textBox_Log.Text += "Which reader is an RFID reader? \r\n";
+
+            int choice  = _selectedIndex;
+            if(choice >= 0 && (choice <= readerNames.Count))
+            {
+                return readerNames[choice];
+            }
+            textBox_Log.Text += "An invalid number has been entered.\r\n";
+            textBox_Log.Text += "-----------------------------------------------\r\n";
+            return null;
+        }
+
+        private void parse_tag(byte[] data)
+        {
+            //var suica = new Suica();
+            //suica.analyzeTransaction(data);
+            //ShowResultData(suica);
+            DataManager.GetInstance().AddHistryList(data);
+            //textBox_Log.Text += "Suica履歴データ：" + BitConverter.ToString(data, 0, 16) + "\r\n";
+            //ShowResultData(suica);
+        }
+        private void setTextLog(string msg)
+        {
+            if (Dispatcher.CheckAccess()) {
+                textBox_Log.Text += msg +"\r\n";
+            }
+            else {
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    textBox_Log.Text += msg + "\r\n";
+                }));
+            }
+        }
+        private void ShowResultData(Suica suica)
+        {
+            setTextLog("時間: " + suica.Date);
+            setTextLog("処理: " + suica.Process);
+            setTextLog("入駅: " + suica.InStationName);
+            setTextLog("出駅: " + suica.OutStationName);
+            setTextLog("InLineCode: " + suica.InStationLineCode);
+            setTextLog("InStationCode: " + suica.InStationCode);
+            setTextLog("OutLineCode: "  + suica.OutStationLineCode);
+            setTextLog("OutStationCode:" + suica.OutStationCode);
+        }
+
+        private void Button_ServiceTest_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
